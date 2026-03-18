ceda0ac28d8e852712630e54963d0a020018d635
Jleoneel <jarrunategui319@utm.edu.ec>
2026-03-18 15:47:58 -0500

    Correccion de red

--- src/server.js
+++ src/server.js
@@ -20,10 +20,25 @@ import distributivoRoutes from "./modules/distributivo/distributivo.routes.js";
 import firmaNotificacionRoutes from "./modules/firmas/firmaNotificacion.routes.js";
 
 const app = express();
-app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
-  credentials: true,
-}));
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  "http://192.168.110.121",
+  "http://192.168.5.117",
+  "http://localhost:5173",
+].filter(Boolean);
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("No permitido por CORS"));
+      }
+    },
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 // Crear carpeta uploads si no existe
@@ -47,7 +62,6 @@ app.use("/api/notificaciones", notificacionesRoutes);
 app.use("/api/firmantes", firmantesRoutes);
 app.use("/api/distributivo", distributivoRoutes);
 app.use("/api/firma-notificaciones", firmaNotificacionRoutes);
-
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`API running on http://localhost:${port}`));
